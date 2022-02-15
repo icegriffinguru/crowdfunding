6016816aabfe03bd21f66770e609d00fb17a51b5
icegriffinguru <icegriffin1122@gmail.com>
2022-02-15 10:47:23 +0300

    0.0.3

--- src/crowdfunding_main.rs
+++ src/crowdfunding_main.rs
@@ -4,12 +4,21 @@ elrond_wasm::imports!();
 
 #[elrond_wasm::contract]
 pub trait Crowdfunding {
-    #[view]
+    #[view(getTarget)]
     #[storage_mapper("target")]
     fn target(&self) -> SingleValueMapper<BigUint>;
 
+    #[view(getDeadline)]
+    #[storage_mapper("deadline")]
+    fn deadline(&self) -> SingleValueMapper<u64>;
+
+    #[view(getDeposit)]
+    #[storage_mapper("deposit")]
+    fn deposit(&self, donor: &ManagedAddress) -> SingleValueMapper<BigUint>;
+
     #[init]
-    fn init(&self, target: BigUint) {
+    fn init(&self, target: BigUint, deadline: u64) {
         self.target().set(&target);
+        self.deadline(). set(&deadline);
     }
 }
