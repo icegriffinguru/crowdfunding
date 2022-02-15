30742edb50f0c1607b15a35b18060fed1d9725aa
icegriffinguru <icegriffin1122@gmail.com>
2022-02-15 10:38:29 +0300

    0.0.2 - add init & test

--- src/crowdfunding_main.rs
+++ src/crowdfunding_main.rs
@@ -4,7 +4,12 @@ elrond_wasm::imports!();
 
 #[elrond_wasm::contract]
 pub trait Crowdfunding {
+    #[view]
+    #[storage_mapper("target")]
+    fn target(&self) -> SingleValueMapper<BigUint>;
+
     #[init]
-    fn init(&self) {
+    fn init(&self, target: BigUint) {
+        self.target().set(&target);
     }
 }
